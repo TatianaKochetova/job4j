9cd770260a506f140e2c2751dcd1a1e64377527f
TatianaKochetova <t.kochetova514@gmail.com>
2020-02-03 15:22:55 -0500

    Максимум из трех чисел [#226840]

---
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class MultiMax {
+    public int max(int first, int second, int third) {
+        if (first > second && first > third) {
+            return first;
+        } else if (first < second && second > third) {
+            return second;
+        } else {
+            return third;
+        }
+    }
+}
