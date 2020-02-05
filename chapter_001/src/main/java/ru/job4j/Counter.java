91a06f00e5799a4236d426fc1c37b481b5b75ee6
TatianaKochetova <t.kochetova514@gmail.com>
2020-02-05 18:46:37 -0500

    Подсчет четных чисел в диапазоне [#226916]

--- chapter_001/src/main/java/ru/job4j/Counter.java
+++ chapter_001/src/main/java/ru/job4j/Counter.java
@@ -1,4 +1,4 @@
-package ru.job4j.loop;
+package ru.job4j;
 
 public class Counter {
     public static int sum(int start, int finish) {
@@ -8,7 +8,15 @@ public class Counter {
         }
         return sum;
     }
-
+    public static int sumByEven(int start, int finish) {
+        int sum = 0;
+        for (int index = start; index <= finish; index++) {
+            if ((index % 2) == 0) {
+                sum = sum + index;
+            }
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
@@ -17,6 +25,9 @@ public class Counter {
         System.out.println(sum(1, 5));
         System.out.println(sum(-1, 3));
 
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
 
     }
 }
