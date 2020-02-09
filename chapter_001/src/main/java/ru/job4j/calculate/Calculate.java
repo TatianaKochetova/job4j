ea62c4283f55d234848694c9312ccb95ba4319d0
TatianaKochetova <t.kochetova514@gmail.com>
2020-02-09 15:13:43 -0500

    JavaDoc. Code convention [#226919]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculate;
+
+/**
+ * Calculate.
+ * @author Kochetova Tatiana (t.kochetova514@gmail.com)
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main (String[] args){
+
+    }
+}
